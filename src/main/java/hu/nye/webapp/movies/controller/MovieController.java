4ab1e664f91ec072c4b73bf7c156f7dee0603ac6
TreszkaiT <treszkaitom@gmail.com>
2022-04-02 12:43:09 +0200

    import hiba kijavitva

--- src/main/java/hu/nye/webapp/movies/controller/MovieController.java
+++ src/main/java/hu/nye/webapp/movies/controller/MovieController.java
@@ -3,13 +3,9 @@ package hu.nye.webapp.movies.controller;
 import hu.nye.webapp.movies.dto.MovieDTO;
 import hu.nye.webapp.movies.entity.Movie;
 import hu.nye.webapp.movies.service.MovieService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
